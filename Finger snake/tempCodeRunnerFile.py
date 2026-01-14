787c898670c8c1098990fbd05dec99efc6315952
HIREN CHANDWANI <136164242+EDDY0502@users.noreply.github.com>
2026-01-14 22:09:09 +0530

    Delete Finger snake directory

--- Finger snake/tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-  for i,points in enumerate(self.points):
-        if i!=0:
-          cv2.line(imgMain,self.points[i-1],self.points[i],(0,0,255),20)
-      cv2.circle(imgMain, self.points[-1], 20, (200,0,200), cv2.FILLED) 
